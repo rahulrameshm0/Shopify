7970352e078ecc37bcb27376404812d7c67a3939
rahulrameshm0 <iamrahulm23@gmail.com>
2026-06-21 14:54:04 +0530

    implemented eye toggle for passwords

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,27 @@
 // Javascript Show/Hide toggle button for password field
 
 let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
 let eyeIcon = document.getElementById('eye-icon');
+let eyeIconConfirm = document.getElementById('eye-icon-2');
 
 eyeIcon.onclick = function(){
     if(password.type === "password"){
         password.type = "text";
         eyeIcon.src = eyeOpensrc;
-    }else {
+
+    } else {
         password.type = "password";
         eyeIcon.src = eyeClosedsrc;
+    }
+}
+
+eyeIconConfirm.onclick = function(){
+    if(confirmPassword.type === "password"){
+        confirmPassword.type = "text";
+        eyeIconConfirm.src = eyeOpensrc;
+    }else {
+        confirmPassword.type = "password";
+        eyeIconConfirm.src = eyeClosedsrc;
     }
 }
