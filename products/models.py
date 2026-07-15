823e71d93e674b8142a2e0af7cbf4e6822b6e4e7
rahulrameshm0 <iamrahulm23@gmail.com>
2026-07-16 03:23:20 +0530

    models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from categories.models import Category
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=260)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=150)
+    price = models.DecimalField(max_length=20, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products/")
+    description = models.TextField()
+
+    def __str__(self):
+        return  self.name
