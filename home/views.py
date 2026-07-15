a6113d42a482aaf9beabb10b72b3caed0932aaa2
rahulrameshm0 <iamrahulm23@gmail.com>
2026-07-15 10:48:48 +0530

    linking the pages

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/home.html")
+
+def products(request):
+    return render(request, "products/products-details.html")
+
+def shop(request):
+    return render(request, "shop/shopping.html")
+
+def cart(request):
+    return render(request, "cart/cart.html")
+
+def contacts(request):
+    return render(request, "contacts/contacts.html")
