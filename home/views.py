15ff7896cbff1120d080a9b1dc8cd072d6243050
rahulrameshm0 <iamrahulm23@gmail.com>
2026-07-17 14:49:43 +0530

    setting up filltering section

--- home/views.py
+++ home/views.py
@@ -1,23 +1,61 @@
 from django.shortcuts import render
 from products.models import Products
+from categories.models import Category
 from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 def home(request):
     return render(request, "home/home.html")
 
+
 def products(request):
     return render(request, "products/products-details.html")
 
+
 def shop(request):
     products = Products.objects.all()
-    paginator = Paginator(products, 4)
 
+    # CATEGORY FILTER
+    categories = request.GET.getlist("category")
+
+    if categories:
+        products = products.filter(category__name__in=categories)
+
+    # BRAND FILTER
+    brands = request.GET.getlist("brand")
+    if brands:
+        products = products.filter(brand__in=brands)
+
+    # PRICE FILTER
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    # AVAILABILITY FILTER
+    availability = request.GET.getlist("availability")
+
+    if "instock" in availability:
+        products = products.filter(stock__gt=0)
+
+    # RATING FILTER
+    rating = request.GET.get("rating")
+
+    if rating:
+        products = products.filter(rating__gte=rating)
+
+    # PAGINATION
+    paginator = Paginator(products, 8)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request,"shop/shopping.html",{"page_obj": page_obj })
-    # return render(request, "shop/shopping.html")
+    return render(request, "shop/shopping.html", {"page_obj": page_obj, "categories": Category.objects.all(), }, )
 
 def cart(request):
     return render(request, "cart/cart.html")
