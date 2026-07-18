af0e8c265cacdba9e49f0e479bc41208e52c050d
rahulrameshm0 <iamrahulm23@gmail.com>
2026-07-18 22:26:52 +0530

    code updated

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('shop/', include('products.urls')),
     path('categories/', include('categories.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
