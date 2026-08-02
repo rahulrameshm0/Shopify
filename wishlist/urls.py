f0033a0dde866dcc4e5564a310646b574ac0db95
rahulrameshm0 <iamrahulm23@gmail.com>
2026-08-02 13:31:19 +0530

    setting up items that can be added to wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path("", views.wishlist, name="wishlist"),
+    path("add/<int:id>/", views.add_to_wishlist, name="add_to_wishlist"),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Wishlist
+from products.models import Products
 # Create your views here.
+
+@login_required(login_url="signin")
+def wishlist(request):
+    wishlist_items = (
+        Wishlist.objects.filter(user=request.user).select_related("products")
+    )
+
+    print("Logged in user:", request.user)
+    print("Wishlist count:", wishlist_items.count())
+
+    for item in wishlist_items:
+        print(item.user, item.products.name)
+
+    context ={
+        "wishlist_items": wishlist_items,
+        "quantity": wishlist_items.count()
+    }
+
+    return render(request, "wishlist/wishlist.html", context)
+
+@login_required(login_url="signin")
+def add_to_wishlist(request, id):
+    product = get_object_or_404(Products, id=id)
+
+    Wishlist.objects.get_or_create(
+        user = request.user,
+        products = product
+    )
+
+    return redirect(request.META.get("HTTP_REFERER", "/"))
