165c14f8c9454b28c6640939afedd027af412e62
rahulrameshm0 <iamrahulm23@gmail.com>
2026-08-11 20:09:18 +0530

    code updated

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+class LoginTest(TestCase):
+
+    def test_login_page_loads(self):
+        response = self.client.get("/signin/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_with_valid_credentials(self):
+        User.objects.create_user(
+            username="rahulrameshm0",
+            password="724850"
+        )
+
+        response = self.client.post("/signin/",
+                                {"username":"rahulrameshm0",
+                                "password":"724850"}
+                                    )
+        self.assertEqual(response.status_code, 302)
+
+
+class RegisterTest(TestCase):
+    def test_register_page_loads(self):
+        response = self.client.get("/register/")
+        self.assertEqual(response.status_code, 200) 
+
+class EmailVarificationTest(TestCase):
+    def test_email_varification_page_loads(self):
+        response = self.client.get("/email_varification/")
+        self.assertEqual(response.status_code, 200)
+        
+class PasswordResetTest(TestCase):
+    def test_password_reset_page_loads(self):
+        response = self.client.get("/password_reset_sent/")
+        self.assertEqual(response.status_code, 200)
